cd5e51127e4e8c9d68c206694da60533ec8b796d
Murat Mavlidov <mmavlidov96@mail.ru>
2019-12-01 12:01:02 +0300

    Updates

---
+++ precache-manifest.ae8dd4e56000d106d0481ab2e46e4f9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee02a7125c312ce342d0b8e45f572d50",
+    "url": "/megapolis/index.html"
+  },
+  {
+    "revision": "d6c42da45b0f10a96333",
+    "url": "/megapolis/static/css/main.d8b4c5a2.chunk.css"
+  },
+  {
+    "revision": "c38c51c1f1bd03567cca",
+    "url": "/megapolis/static/js/2.660d5ff7.chunk.js"
+  },
+  {
+    "revision": "d6c42da45b0f10a96333",
+    "url": "/megapolis/static/js/main.17e83dfe.chunk.js"
+  },
+  {
+    "revision": "c57aefadade2e1d2ba6c",
+    "url": "/megapolis/static/js/runtime-main.515078ea.js"
+  }
+]);
